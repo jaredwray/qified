b0656bc074443d52202d0bba65d4bf5336ed95c4
Jared Wray <me@jaredwray.com>
2024-11-30 08:55:37 -0800

    updating site information

--- site/docula.config.mjs
+++ site/docula.config.mjs
@@ -14,7 +14,7 @@ export const onPrepare = async config => {
 	const readmePath = path.join(process.cwd(), './README.md');
 	const readmeSitePath = path.join(config.sitePath, 'README.md');
 	const readme = await fs.promises.readFile(readmePath, 'utf8');
-	const updatedReadme = readme.replace('<img src="site/logo.svg" alt="Hookified" height="400" align="center">\n\n', '');
+	const updatedReadme = readme.replace('![site/logo.svg](site/logo.svg)', '');
 	console.log('writing updated readme to', readmeSitePath);
 	await fs.promises.writeFile(readmeSitePath, updatedReadme);
 };
