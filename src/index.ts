dfe7105ba3799d8ca5cbbfbfa7a5c90eb53d1806
Jared Wray <me@jaredwray.com>
2024-11-30 09:08:49 -0800

    adding in some tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 
+export type QifiedOptions = {
+	/**
+	 * The base uri to use for all services.
+	 */
+	uris: string[];
+	/**
+	 * The default channel to send messages to.
+	 */
+	defaultChannel: string;
+};
+
 export class Qified {
-	constructor() {
-		console.log('Qified constructor');
+	private _defaultChannel: string;
+	private _uris: string[];
+	constructor(options?: QifiedOptions) {
+		this._defaultChannel = options?.defaultChannel ?? 'default';
+		this._uris = options?.uris ?? [];
+	}
+
+	get defaultChannel() {
+		return this._defaultChannel;
+	}
+
+	set defaultChannel(channel: string) {
+		this._defaultChannel = channel;
+	}
+
+	get uris() {
+		return this._uris;
+	}
+
+	set uris(uris: string[]) {
+		this._uris = uris;
 	}
 }
---
+++ test/index.test.ts
@@ -0,0 +1,32 @@
+import {describe, test, expect} from 'vitest';
+import {Qified} from '../src/index.js';
+
+describe('Qified', () => {
+	test('constructor', () => {
+		const qified = new Qified();
+		expect(qified).toBeInstanceOf(Qified);
+	});
+
+	test('constructor with options', () => {
+		const qified = new Qified({
+			defaultChannel: 'new',
+			uris: ['redis://localhost:6379'],
+		});
+		expect(qified.defaultChannel).toBe('new');
+		expect(qified.uris).toEqual(['redis://localhost:6379']);
+	});
+
+	test('defaultChannel', () => {
+		const qified = new Qified();
+		expect(qified.defaultChannel).toBe('default');
+		qified.defaultChannel = 'new';
+		expect(qified.defaultChannel).toBe('new');
+	});
+
+	test('uris', () => {
+		const qified = new Qified();
+		expect(qified.uris).toEqual([]);
+		qified.uris = ['redis://localhost:6379'];
+		expect(qified.uris).toEqual(['redis://localhost:6379']);
+	});
+});
