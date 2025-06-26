7b4577274815be97ed2e52dc613dfa49509a9759
Jared Wray <me@jaredwray.com>
2025-06-25 17:43:52 -0700

    Update index.ts

--- packages/qified/src/index.ts
+++ packages/qified/src/index.ts
@@ -14,16 +14,28 @@ export type QifiedOptions = {
 
 export class Qified {
 	private _messageProviders: MessageProvider[] = [];
+	/**
+	 * Creates an instance of Qified.
+	 * @param {QifiedOptions} options - Optional configuration for Qified.
+	 */
 	constructor(options?: QifiedOptions) {
 		if (options?.messageProviders) {
 			this._messageProviders = options.messageProviders;
 		}
 	}
 
+	/**
+	 * Gets or sets the message providers.
+	 * @returns {MessageProvider[]} The array of message providers.
+	 */
 	public get messageProviders(): MessageProvider[] {
 		return this._messageProviders;
 	}
 
+	/**
+	 * Sets the message providers.
+	 * @param {MessageProvider[]} providers - The array of message providers to set.
+	 */
 	public set messageProviders(providers: MessageProvider[]) {
 		this._messageProviders = providers;
 	}
