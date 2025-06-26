658ca6844353a1b84e1ce5f3964b5c85425fefa9
Jared Wray <me@jaredwray.com>
2025-06-25 17:28:30 -0700

    qified - feat: adding in id for TopicHandlers

--- packages/qified/src/memory/message.ts
+++ packages/qified/src/memory/message.ts
@@ -1,17 +1,17 @@
 import type {Message, MessageProvider, TopicHandler} from '../types.js';
 
 export class MemoryMessageProvider implements MessageProvider {
-	private _subscriptions: TopicHandler[];
+	private _subscriptions: Map<string, TopicHandler[]>;
 
 	constructor() {
-		this._subscriptions = [];
+		this._subscriptions = new Map();
 	}
 
-	public get subscriptions(): TopicHandler[] {
+	public get subscriptions(): Map<string, TopicHandler[]> {
 		return this._subscriptions;
 	}
 
-	public set subscriptions(value: TopicHandler[]) {
+	public set subscriptions(value: Map<string, TopicHandler[]>) {
 		this._subscriptions = value;
 	}
 
@@ -19,22 +19,33 @@ export class MemoryMessageProvider implements MessageProvider {
 	public async init(): Promise<void> {}
 
 	public async publish(topic: string, message: Message): Promise<void> {
-		const subscriptions = this._subscriptions.filter(subscription => subscription.topic === topic);
+		const subscriptions = this._subscriptions.get(topic) ?? [];
 		for (const subscription of subscriptions) {
 			// eslint-disable-next-line no-await-in-loop
 			await subscription.handler(message);
 		}
 	}
 
-	public async subscribe(subscription: TopicHandler): Promise<void> {
-		this._subscriptions.push(subscription);
+	public async subscribe(topic: string, handler: TopicHandler): Promise<void> {
+		if (!this._subscriptions.has(topic)) {
+			this._subscriptions.set(topic, []);
+		}
+
+		this._subscriptions.get(topic)?.push(handler);
 	}
 
-	public async unsubscribe(topic: string): Promise<void> {
-		this._subscriptions = this._subscriptions.filter(subscription => subscription.topic !== topic);
+	public async unsubscribe(topic: string, id?: string): Promise<void> {
+		if (id) {
+			const subscriptions = this._subscriptions.get(topic);
+			if (subscriptions) {
+				this._subscriptions.set(topic, subscriptions.filter(sub => sub.id !== id));
+			}
+		} else {
+			this._subscriptions.delete(topic);
+		}
 	}
 
 	public async disconnect(): Promise<void> {
-		this._subscriptions = [];
+		this._subscriptions.clear();
 	}
 }
