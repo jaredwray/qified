4ce95f3d8825eb56e14c92bc25b361b66453a952
Jared Wray <me@jaredwray.com>
2025-08-07 09:09:09 -0700

    mono - migrating all project over to use biome from xojs

--- packages/qified/test/memory.test.ts
+++ packages/qified/test/memory.test.ts
@@ -1,87 +1,80 @@
+// biome-ignore-all lint/suspicious/noExplicitAny: This is a test file and explicit any is acceptable here.
+import { describe, expect, test } from "vitest";
+import { MemoryMessageProvider } from "../src/memory/message.js";
+import type { Message, TopicHandler } from "../src/types.js";
 
-import {describe, expect, test} from 'vitest';
-import {MemoryMessageProvider} from '../src/memory/message.js';
-import type {Message, TopicHandler} from '../src/types.js';
-
-describe('MemoryMessageProvider', () => {
-	test('should initialize with empty subscriptions', async () => {
+describe("MemoryMessageProvider", () => {
+	test("should initialize with empty subscriptions", async () => {
 		const provider = new MemoryMessageProvider();
 		expect(provider.subscriptions).toEqual(new Map());
 	});
 
-	test('should be able to set subscriptions', () => {
+	test("should be able to set subscriptions", () => {
 		const provider = new MemoryMessageProvider();
 		const subscriptions = new Map<string, TopicHandler[]>();
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		subscriptions.set('test/topic1', [{async handler(message: any) {}}]);
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		subscriptions.set('test/topic2', [{async handler(message: any) {}}]);
+		subscriptions.set("test/topic1", [{ async handler(_message: any) {} }]);
+		subscriptions.set("test/topic2", [{ async handler(_message: any) {} }]);
 		provider.subscriptions = subscriptions;
 		expect(provider.subscriptions).toEqual(subscriptions);
 	});
 
-	test('should add a subscription', async () => {
+	test("should add a subscription", async () => {
 		const provider = new MemoryMessageProvider();
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		const handler = async (message: any) => {};
-		await provider.subscribe('test/topic', {id: 'test', handler});
+		const handler = async (_message: any) => {};
+		await provider.subscribe("test/topic", { id: "test", handler });
 		expect(provider.subscriptions.size).toBe(1);
-		expect(provider.subscriptions.get('test/topic')?.[0].id).toBe('test');
+		expect(provider.subscriptions.get("test/topic")?.[0].id).toBe("test");
 	});
 
-	test('should add multiple to a subscription', async () => {
+	test("should add multiple to a subscription", async () => {
 		const provider = new MemoryMessageProvider();
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		const handler = async (message: any) => {};
-		await provider.subscribe('test/topic', {id: 'test', handler});
-		await provider.subscribe('test/topic', {id: 'test2', handler});
+		const handler = async (_message: any) => {};
+		await provider.subscribe("test/topic", { id: "test", handler });
+		await provider.subscribe("test/topic", { id: "test2", handler });
 		expect(provider.subscriptions.size).toBe(1);
-		expect(provider.subscriptions.get('test/topic')?.[0].id).toBe('test');
-		expect(provider.subscriptions.get('test/topic')?.[1].id).toBe('test2');
-		expect(provider.subscriptions.get('test/topic')?.length).toBe(2);
+		expect(provider.subscriptions.get("test/topic")?.[0].id).toBe("test");
+		expect(provider.subscriptions.get("test/topic")?.[1].id).toBe("test2");
+		expect(provider.subscriptions.get("test/topic")?.length).toBe(2);
 	});
 
-	test('should remove a subscription', async () => {
+	test("should remove a subscription", async () => {
 		const provider = new MemoryMessageProvider();
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		const handler = async (message: any) => {};
-		await provider.subscribe('test/topic', {id: 'test', handler});
+		const handler = async (_message: any) => {};
+		await provider.subscribe("test/topic", { id: "test", handler });
 		expect(provider.subscriptions.size).toBe(1);
-		await provider.unsubscribe('test/topic', 'test');
-		expect(provider.subscriptions.get('test/topic')?.length).toBe(0);
+		await provider.unsubscribe("test/topic", "test");
+		expect(provider.subscriptions.get("test/topic")?.length).toBe(0);
 	});
 
-	test('should remove a subscription without id', async () => {
+	test("should remove a subscription without id", async () => {
 		const provider = new MemoryMessageProvider();
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		const handler = async (message: any) => {};
-		await provider.subscribe('test/topic', {id: 'test', handler});
+		const handler = async (_message: any) => {};
+		await provider.subscribe("test/topic", { id: "test", handler });
 		expect(provider.subscriptions.size).toBe(1);
-		await provider.unsubscribe('test/topic');
-		expect(provider.subscriptions.get('test/topic')).toBe(undefined);
+		await provider.unsubscribe("test/topic");
+		expect(provider.subscriptions.get("test/topic")).toBe(undefined);
 	});
 
-	test('should publish a message to the correct topic', async () => {
+	test("should publish a message to the correct topic", async () => {
 		const provider = new MemoryMessageProvider();
-		const message: Message = {id: 'foo', data: {test: 'message'}};
-		let handlerMessage = '';
+		const message: Message = { id: "foo", data: { test: "message" } };
+		let handlerMessage = "";
 		const handler = async (message: any) => {
 			expect(message).toEqual(message);
 			handlerMessage = message as string;
 		};
 
-		await provider.subscribe('test/topic', {id: 'test', handler});
+		await provider.subscribe("test/topic", { id: "test", handler });
 
-		await provider.publish('test/topic', message);
+		await provider.publish("test/topic", message);
 
 		expect(handlerMessage).toEqual(message);
 	});
 
-	test('should disconnect and clear subscriptions', async () => {
+	test("should disconnect and clear subscriptions", async () => {
 		const provider = new MemoryMessageProvider();
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		const handler = async (message: any) => {};
-		await provider.subscribe('test/topic', {id: 'test', handler});
+		const handler = async (_message: any) => {};
+		await provider.subscribe("test/topic", { id: "test", handler });
 		expect(provider.subscriptions.size).toBe(1);
 		await provider.disconnect();
 		expect(provider.subscriptions.size).toBe(0);
