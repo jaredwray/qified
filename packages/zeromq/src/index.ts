301bcdc732a70339c0b2d6721e72a62ac2d827f3
Santosh Bandichode <santoshg550@gmail.com>
2025-09-29 17:00:21 +0530

    Add ZeroMQ provider

---
+++ packages/zeromq/src/index.ts
@@ -0,0 +1,134 @@
+import { Buffer } from "node:buffer";
+import {
+	type Message,
+	type MessageProvider,
+	Qified,
+	type TopicHandler,
+} from "qified";
+import { Publisher, Subscriber } from "zeromq";
+
+export type ZmqMessageProviderOptions = {
+	uri?: string;
+};
+
+export const defaultZmqUri = "tcp://localhost:3000";
+
+export class ZmqMessageProvider implements MessageProvider {
+	public subscriptions = new Map<string, TopicHandler[]>();
+
+	private _subscriber: Subscriber | undefined;
+	private _publisher: Publisher | undefined;
+	private _uri: string;
+	private _awaitingMessages: boolean;
+
+	constructor(options: ZmqMessageProviderOptions = {}) {
+		this._uri = options.uri ?? defaultZmqUri;
+		this._awaitingMessages = false;
+	}
+
+	private async _createConnection(): Promise<void> {
+		// Publisher `binds` and subscriber `connects`
+
+		if (!this._publisher) {
+			this._publisher = new Publisher();
+			await this._publisher.bind(this._uri);
+		}
+
+		if (!this._subscriber) {
+			this._subscriber = new Subscriber();
+			this._subscriber.connect(this._uri);
+		}
+	}
+
+	/**
+	 * Publishes a message to a specified topic.
+	 * @param {string} topic The topic to publish the message to.
+	 * @param {Message} message The message to publish.
+	 * @returns {Promise<void>} A promise that resolves when the message is published.
+	 */
+	public async publish(topic: string, message: Message): Promise<void> {
+		await this._createConnection();
+		await this._publisher?.send([topic, Buffer.from(JSON.stringify(message))]);
+	}
+
+	/**
+	 * Subscribes to a specified topic.
+	 * @param {string} topic The topic to subscribe to.
+	 * @param {TopicHandler} handler The handler to process incoming messages.
+	 */
+	public async subscribe(topic: string, handler: TopicHandler): Promise<void> {
+		await this._createConnection();
+
+		if (!this.subscriptions.has(topic)) {
+			this.subscriptions.set(topic, []);
+			this._subscriber?.subscribe(topic);
+		}
+
+		this.subscriptions.get(topic)?.push(handler);
+
+		if (!this._awaitingMessages) {
+			(async () => {
+				for await (const [tpc, msg] of this._subscriber as Subscriber) {
+					if (msg) {
+						const message = JSON.parse(msg.toString());
+						const handlers = this.subscriptions.get(tpc.toString()) ?? [];
+						await Promise.all(
+							handlers.map(async (sub) => sub.handler(message)),
+						);
+					}
+				}
+			})();
+
+			this._awaitingMessages = true;
+		}
+	}
+
+	/**
+	 * Unsubscribes from a specified topic.
+	 * @param {string} topic The topic to unsubscribe from.
+	 * @param {string} [id] Optional identifier for the subscription to remove.
+	 * @returns {Promise<void>} A promise that resolves when the unsubscription is complete.
+	 */
+	public async unsubscribe(topic: string, id?: string): Promise<void> {
+		if (id) {
+			const current = this.subscriptions.get(topic);
+			if (current) {
+				this.subscriptions.set(
+					topic,
+					current.filter((sub) => sub.id !== id),
+				);
+			}
+		} else {
+			this.subscriptions.delete(topic);
+		}
+	}
+
+	/**
+	 * Disconnects from the ZeroMQ server and cancels all subscriptions.
+	 * @returns {Promise<void>} A promise that resolves when the disconnection is complete.
+	 */
+	public async disconnect(): Promise<void> {
+		const topics = this.subscriptions.keys();
+		for (const topic of topics) {
+			this._subscriber?.unsubscribe(topic);
+		}
+
+		this.subscriptions.clear();
+
+		this._subscriber?.close();
+		this._publisher?.close();
+
+		this._subscriber = undefined;
+		this._publisher = undefined;
+	}
+}
+
+/**
+ * Creates a new instance of Qified with a ZeroMQ message provider.
+ * @param {ZmqMessageProviderOptions} options Optional configuration for the ZeroMQ message provider.
+ * @returns A new instance of Qified.
+ */
+export function createQified(options?: ZmqMessageProviderOptions): Qified {
+	const provider = new ZmqMessageProvider(options);
+	return new Qified({ messageProviders: [provider] });
+}
---
+++ packages/zeromq/test/index.test.ts
@@ -0,0 +1,122 @@
+import { type Message, Qified } from "qified";
+import { describe, expect, test } from "vitest";
+import { createQified, ZmqMessageProvider } from "../src/index.js";
+
+describe("ZmqMessageProvider", () => {
+	test("should create an instance", () => {
+		const provider = new ZmqMessageProvider();
+		expect(provider).toBeInstanceOf(ZmqMessageProvider);
+		expect(provider.subscriptions.size).toBe(0);
+	});
+
+	test("should publish and receive a message", async () => {
+		const provider = new ZmqMessageProvider();
+		const message: Message = { id: "1", data: "test" };
+		let received: Message | undefined;
+		const id = "test-handler";
+		await provider.subscribe("test-topic", {
+			id,
+			async handler(message) {
+				received = message;
+			},
+		});
+
+		// Let the event loop iterate so message queue is read/written at next tick
+		await new Promise<void>((resolve) => {
+			setTimeout(resolve, 100);
+		});
+
+		await provider.publish("test-topic", message);
+
+		// Let the event loop iterate so message queue is read/written at next tick
+		await new Promise<void>((resolve) => {
+			setTimeout(resolve, 100);
+		});
+
+		expect(received).toEqual(message);
+
+		await provider.unsubscribe("test-topic", id);
+		await provider.disconnect();
+	});
+
+	test("should unsubscribe all handlers with no id", async () => {
+		const provider = new ZmqMessageProvider();
+		const message: Message = { id: "1", data: "test" };
+		let received1: Message | undefined;
+		let received2: Message | undefined;
+		await provider.subscribe("test-topic", {
+			async handler(message) {
+				received1 = message;
+			},
+		});
+
+		await provider.subscribe("test-topic", {
+			async handler(message) {
+				received2 = message;
+			},
+		});
+
+		// Let the event loop iterate so message queue is read/written at next tick
+		await new Promise<void>((resolve) => {
+			setTimeout(resolve, 100);
+		});
+
+		await provider.publish("test-topic", message);
+
+		// Let the event loop iterate so message queue is read/written at next tick
+		await new Promise<void>((resolve) => {
+			setTimeout(resolve, 100);
+		});
+
+		const firstSubscriptions = provider.subscriptions.get("test-topic");
+		expect(firstSubscriptions?.length).toBe(2);
+
+		expect(received1).toEqual(message);
+		expect(received2).toEqual(message);
+
+		await provider.unsubscribe("test-topic");
+
+		const subscriptions = provider.subscriptions.get("test-topic");
+		expect(subscriptions).toBeUndefined();
+
+		await provider.disconnect();
+	});
+
+	test("should be able to use with Qified", async () => {
+		const provider = new ZmqMessageProvider();
+		const qified = new Qified({ messageProviders: [provider] });
+		const message: Message = { id: "1", data: "test" };
+		let received: Message | undefined;
+		const id = "test-handler";
+		await qified.subscribe("test-topic", {
+			id,
+			async handler(message) {
+				received = message;
+			},
+		});
+
+		// Let the event loop iterate so message queue is read/written at next tick
+		await new Promise<void>((resolve) => {
+			setTimeout(resolve, 100);
+		});
+
+		await qified.publish("test-topic", message);
+
+		// Let the event loop iterate so message queue is read/written at next tick
+		await new Promise<void>((resolve) => {
+			setTimeout(resolve, 100);
+		});
+
+		expect(received).toEqual(message);
+
+		await qified.unsubscribe("test-topic", id);
+		await qified.disconnect();
+	});
+
+	test("should create Qified instance with ZeroMQ provider", () => {
+		const qified = createQified();
+		expect(qified).toBeInstanceOf(Qified);
+		expect(qified.messageProviders.length).toBe(1);
+		expect(qified.messageProviders[0]).toBeInstanceOf(ZmqMessageProvider);
+	});
+});
