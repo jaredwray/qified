8d61f8adc1d680961dd04e12659c1e23eec08a85
Jared Wray <me@jaredwray.com>
2026-03-08 09:36:09 -0600

    moving empty listeners to false

--- packages/redis/src/task.ts
+++ packages/redis/src/task.ts
@@ -60,7 +60,7 @@ export class RedisTaskProvider extends Hookified implements TaskProvider {
 	 * @param options Configuration options for the provider
 	 */
 	constructor(options: RedisTaskProviderOptions = {}) {
-		super();
+		super({ throwOnEmptyListeners: false });
 		const uri = options.uri ?? defaultRedisUri;
 		this._id = options.id ?? defaultRedisTaskId;
 		this._timeout = options.timeout ?? defaultTimeout;
